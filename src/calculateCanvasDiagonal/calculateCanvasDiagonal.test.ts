20ca7878876e9fe3c69adeaf41483d8eda96e0c1
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-26 14:50:18 -0400

    Refactor: Update the title for test2

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -4,7 +4,7 @@ describe('calculateCanvasDiagonal', () => {
   it('Verify that calculateCanvasDiagonal should calculate the correct diagonal when length is positive and width is zero', () => {
     expect(calculateCanvasDiagonal('3', '0')).toBe(6);
   });
-  it ('Verify that calculateCanvasSize should calculate perimeter correctly when length is zero and width is positive', () => {
+  it ('Verify that calculateCanvasDiagonal should calculate the correct diagonal when length is zero and width is positive', () => {
     expect(calculateCanvasDiagonal('0', '4')).toBe(8);
   });
   it('Verify that calculateCanvasSize should handle negative length correctly (absolute value)', () => {
