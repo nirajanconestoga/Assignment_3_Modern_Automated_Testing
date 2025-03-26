efb4f41a8986c6cdf2f9b746a46e298d012530fd
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-26 15:12:05 -0400

    Refactored: Update the result of test 1 to match the result after upfating the production code

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -2,7 +2,7 @@ import calculateCanvasDiagonal from './calculateCanvasDiagonal';
 
 describe('calculateCanvasDiagonal', () => {
   it('Verify that calculateCanvasDiagonal should calculate the correct diagonal when length is positive and width is zero', () => {
-    expect(calculateCanvasDiagonal('3', '0')).toBe(6);
+    expect(calculateCanvasDiagonal('3', '0')).toBe(3);
   });
   it ('Verify that calculateCanvasDiagonal should calculate the correct diagonal when length is zero and width is positive', () => {
     expect(calculateCanvasDiagonal('0', '4')).toBe(8);
