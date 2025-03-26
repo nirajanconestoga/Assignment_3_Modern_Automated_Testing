6dcd0087678a943f81455af6a8a4d8694bfd05f6
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-26 15:09:38 -0400

    Refactored: Updated the production code to correctly calculate the diagonal of canvas

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
@@ -1,6 +1,3 @@
-export default function calculateCanvasSize(
-  length: string,
-  width: string
-) {
-  return 2 * (parseFloat(length) + parseFloat(width));
+export default function calculateCanvasDiagonal(length: string, width: string) {
+  return Math.sqrt(Math.pow(parseFloat(length), 2) + Math.pow(parseFloat(width), 2));
 }
