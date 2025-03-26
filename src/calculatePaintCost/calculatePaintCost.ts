4e70805913e97f2b63df2096c155818b7b9ec945
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-26 15:30:07 -0400

    Refactor: Update calculatePaintCost to return 0 for negative values of paintRequired or costPerLiter

--- src/calculatePaintCost/calculatePaintCost.ts
+++ src/calculatePaintCost/calculatePaintCost.ts
@@ -1,3 +1,6 @@
 export default function calculatePaintCost(paintRequired: number, costPerLiter: number): number {
+  if (paintRequired < 0 || costPerLiter < 0) {
+    return 0;
+  }
   return paintRequired * costPerLiter;
 }
